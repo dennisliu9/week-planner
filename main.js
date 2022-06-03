fbea4549a51e7f67d2ca777e260160e8a1e8fb2e
sharon-tieu <tieusharon@gmail.com>
2022-06-03 20:26:38 +0000

    Wrote eventListeners for buttons and started on data model. gg :3

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+var $modal = document.querySelector('.modal-screen');
 var $entryButton = document.querySelector('#add-entry');
+var $modalScreen = document.querySelector('.modal-screen');
+var $submit = document.querySelector('#submit-button');
+
+var data = {
+  Monday: [],
+  Tuesday: [],
+  Wednesday: [],
+  Thursday: [],
+  Friday: [],
+  Saturday: [],
+  Sunday: []
+};
+
 $entryButton.addEventListener('click', function (event) {
+  $modal.classList.remove('invisible');
+  // console.log('button working :P', $entryButton);
+  // console.log($modal.classList);
 });
+
+$submit.addEventListener('click', function (event) {
+  $modal.classList.add('invisible');
+});
+
+// $modalScreen.addEventListener('click', function (event) {
+//   $modal.classList.add('invisible');
+// });
